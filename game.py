7f77cbbcfa3e9c0bd6d55a7c25cfd618e1a05003
anastaysha15 <115075080+anastaysha15@users.noreply.github.com>
2022-12-07 13:30:04 +0300

    Update game.py
    Комментарии к функциям

--- game.py
+++ game.py
@@ -9,9 +9,8 @@ class Game:
         self.clock = pygame.time.Clock()
         self.screen = screen
         pygame.display.set_caption('Amoeba Jump')
-        
 
-   # Отрисовка шрифтов
+        # Отрисовка шрифтов
         self.high_score = 0
         self.big_font = pygame.freetype.SysFont('Arial', 75)
         self.middle_font = pygame.freetype.SysFont('Arial', 40)
@@ -20,7 +19,6 @@ class Game:
         self.on_ground = True
         self.speed = 0
         self.player = None
-
 
     def draw_author(self):
         text_surface0, rect = self.small_font.render(f' The game starts when you press the "play" key.', (0, 0, 0))
@@ -39,8 +37,6 @@ class Game:
         self.screen.blit(text_surface5, (170, 370))
         self.screen.blit(text_surface6, (170, 395))
         self.screen.blit(text_surface7, (310, 450))
-       
-        
 
     def draw_menu_header(self):
 
@@ -58,7 +54,7 @@ class Game:
             ),
         )
 
-   # Отрисовка окаймлающих прямоугольников в меню
+    # Отрисовка окаймлающих прямоугольников в меню
     def draw_overlay(self):
         pygame.draw.rect(self.screen, (123, 104, 238), (0, 0, 150, 800), 0)
         pygame.draw.rect(self.screen, (123, 104, 238), (650, 0, 150, 800), 0)
@@ -70,7 +66,7 @@ class Game:
         self.screen.blit(text_surface, (20, 50))
 
     def boundaries(self, platforms):
-    # Обработка касаний между платформой и игроком
+        # Обработка касаний между платформой и игроком
         for platform in platforms.sprites():
             # Условия касания с правой и левой границей, проверка нахождения игрока выше верхней поверхности платформы
             if (
@@ -86,15 +82,14 @@ class Game:
         if not self.on_ground:
             self.speed += 1
 
-    def game(self, r = 140):
-        '''
-        это основная функция иии
+    def game(self, r=140):
+        '''это основная функция иии
         :param power: сила прыжка ...
-        :return:
+        :
         '''
         game_run = True
         # Переменная power отвечает за силу прыжка
-        next_level = 10
+
         power = 15
         self.player = sprites.Sprite(400, 500, 50, 50, 'amoeba.png')
         score = 0
@@ -109,13 +104,13 @@ class Game:
         fail = False
 
         while game_run:
-           # Перемещение персонажа вправо и влево
+            # Перемещение персонажа вправо и влево
             keys = pygame.key.get_pressed()
             if keys[pygame.K_RIGHT]:
-                self.player.rect.x += 10
+                self.player.rect.x += 13
 
             if keys[pygame.K_LEFT]:
-                self.player.rect.x -= 10
+                self.player.rect.x -= 13
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -127,7 +122,7 @@ class Game:
 
             self.player.rect.y += self.speed
 
-           # Увеличение количества платформ, задание размера платформы (длина,ширина)
+            # Увеличение количества платформ, задание размера платформы (длина, r = ширина)
             if upper_platform.rect.y > power * 10:
                 upper_platform = sprites.Sprite(random.randint(217, 550), 0, r, 40, 'plat.png')
                 platforms.add(upper_platform)
@@ -136,8 +131,8 @@ class Game:
                 # Если значение скорости меньше 0 и если игрок вблизи 300 по у, то платформы должны двигаться вниз
                 if self.speed < 0:
                     if self.player.rect.y < 300:
-                        platform.rect.y -= self.speed * 0.5
-                    platform.rect.y -= self.speed
+                        platform.rect.y -= self.speed
+                    platform.rect.y -= self.speed * 0.5
                 if platform.rect.y >= 800:
                     platform.kill()
                     # Добавление очков в сслучае уничтожения платформы, когда она выходит за границу экрана
@@ -157,10 +152,6 @@ class Game:
                     self.high_score = score
                 game_run = False
                 fail = True
-
-            if score > next_level:
-                next_level += 15
-                power += 0.5
 
             pygame.display.flip()
             self.clock.tick(60)
